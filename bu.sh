67f55812784be0edc2776dfb7602a17a42bc0bd7
黄荣卫 <huangrongwei@cmhi.chinamobile.com>
2018-12-29 12:25:06 +0800

    完善hashmap亦或hash算法

--- bu.sh
+++ bu.sh
@@ -1,5 +1,7 @@
 
-bash ./configure --enable-debug --with-target-bits=64 --with-jobs=8 --disable-warnings-as-errors --with-jvm-variants=server
+//bash ./configure --enable-debug --with-target-bits=64 --with-jobs=8 --disable-warnings-as-errors --with-jvm-variants=server
 
+
+./configure --enable-debug --with-target-bits=64 --with-jobs=8 --disable-warnings-as-errors --with-jvm-variants=server --with-freetype-include=/usr/local/include/freetype2 --with-freetype-lib=/usr/local/lib/
 
 make
